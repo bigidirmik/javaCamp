30cd6b6c2634cc0c6aaaef196d989fa96eaa477d
Bilal Dırmık <bigisivri@outlook.com>
2021-05-25 17:17:19 +0300

    Swagger ve Core.Results

--- hrms/src/main/java/kodlamaio/hrms/business/abstracts/JobTitleService.java
+++ hrms/src/main/java/kodlamaio/hrms/business/abstracts/JobTitleService.java
@@ -2,8 +2,12 @@ package kodlamaio.hrms.business.abstracts;
 
 import java.util.List;
 
+import kodlamaio.hrms.core.utilities.results.DataResult;
+import kodlamaio.hrms.core.utilities.results.Result;
 import kodlamaio.hrms.entities.concretes.JobTitle;
 
 public interface JobTitleService {
-	List<JobTitle> getAll();
+	Result add(JobTitle jobTitle);
+	
+	DataResult<List<JobTitle>> getAll();
 }
--- hrms/src/main/java/kodlamaio/hrms/entities/concretes/JobTitle.java
+++ hrms/src/main/java/kodlamaio/hrms/entities/concretes/JobTitle.java
@@ -3,32 +3,27 @@ package kodlamaio.hrms.entities.concretes;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
-@Data
 @Entity
 @Table(name="job_titles")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class JobTitle {
 	
 	@Id
-	@GeneratedValue
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	@Column(name="id")
 	private int id;
 	
 	@Column(name="title")
 	private String title;
-	
-	public JobTitle() {
-		// TODO Auto-generated constructor stub
-	}
-	
-	public JobTitle(int id, String title) {
-		super();
-		this.id = id;
-		this.title = title;
-	}
 
 }
